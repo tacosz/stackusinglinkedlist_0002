c33a76c18ec86d65f94735b834966fbaba784fe5
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-11 10:18:27 +0700

    menampilkan dan menyimpan data value

--- StackUsingLinkedList/StackUsingLinkedList.cpp
+++ StackUsingLinkedList/StackUsingLinkedList.cpp
@@ -32,6 +32,8 @@ public:
         newNode->data = value; // 2. assign value
         newNode->next = top; // 3. set the next pointer of the new node to the current top node
         top = newNode; // 4. Update the top pointer to the new node
+        cout << "Push value: " << value << endl;
+        return value;
     }
 };
 
