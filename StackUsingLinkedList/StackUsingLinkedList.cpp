554723fcfc3c6fc523070e7b29af9a49005d69d0
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-11 09:22:16 +0700

    fungsi class node

--- StackUsingLinkedList/StackUsingLinkedList.cpp
+++ StackUsingLinkedList/StackUsingLinkedList.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+using namespace std;
+
+// Node class representing a single node in the linked list
+class Node
+{
+public:
+    int data;
+    Node* next;
+
+    Node() {
+        next = NULL;
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
