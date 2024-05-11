ca745bbcc21c9721e3f4aa0b2eb8302c25949929
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-11 10:26:29 +0700

    menampilkan popped value kemudian di update top ke next

--- StackUsingLinkedList/StackUsingLinkedList.cpp
+++ StackUsingLinkedList/StackUsingLinkedList.cpp
@@ -43,6 +43,8 @@ public:
         {
             cout << "Stack is empty." << endl;
         }
+        cout << "Popped value: " << top->data << endl;
+        top = top->next; // Update the top pointer to the next node
     }
 };
 
