3f6ff505f325f28cc78ca4734d100a03fa8a45a7
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-11 10:32:29 +0700

    fungsi else dan while

--- StackUsingLinkedList/StackUsingLinkedList.cpp
+++ StackUsingLinkedList/StackUsingLinkedList.cpp
@@ -54,6 +54,16 @@ public:
         {
             cout << "List is empty." << endl;
         }
+        else 
+        {
+            Node* current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }// Return the value of the top node
     }
 };
 
