dc5306dad14ab6076d79eafc6aaf888ffb08f53d
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-11 10:10:41 +0700

    menambahkan nilai

--- StackUsingLinkedList/StackUsingLinkedList.cpp
+++ StackUsingLinkedList/StackUsingLinkedList.cpp
@@ -29,6 +29,7 @@ public:
     int push(int value)
     {
         Node* newNode = new Node(); // 1. Allocate memory for the new node
+        newNode->data = value; // 2. assign value
         
     }
 };
