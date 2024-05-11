b1490ab3ca4232bc87e31e0b8361e40ef2fb5439
tacosz <nabilnasruddin.offc@gmail.com>
2024-05-11 10:42:49 +0700

    menampilkan beberapa opsi pilihan dan menyimpan dalam variable choice

--- StackUsingLinkedList/StackUsingLinkedList.cpp
+++ StackUsingLinkedList/StackUsingLinkedList.cpp
@@ -75,5 +75,18 @@ public:
 
 int main()
 {
-    
+    Stack stack;
+
+    int choice = 0;
+    int value;
+
+    while (choice != 5)
+    {
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peek\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+    }
 }
